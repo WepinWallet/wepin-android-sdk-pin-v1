537dcb2ee87c3850e6e41f18fadcc18844b6941f
js.hong@iotrust.kr <js.hong@iotrust.kr>
2025-04-07 14:24:21 +0900

    fix login library version

--- pinLib/build.gradle.kts
+++ pinLib/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation("io.wepin:wepin-android-sdk-modal-v1:${sdkVersion}")
     implementation("io.wepin:wepin-android-sdk-storage-v1:${sdkVersion}")
     implementation("io.wepin:wepin-android-sdk-session-v1:${sdkVersion}")
-    api("com.github.WepinWallet:wepin-android-sdk-login-v1:v1.1.0-test2")
+    api("com.github.WepinWallet:wepin-android-sdk-login-v1:v${sdkVersion}")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
